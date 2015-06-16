dcec22300b62d950ed5b66e808fb3ea8646cb1db
Balaji1988 <balaji.kailash@yahoo.com>
2015-06-16 21:59:34 +0530

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,16 +1,8 @@
-SHELL := /bin/bash
-VENV=.
-NO_COLOR    = \x1b[0m
-OK_COLOR    = \x1b[32;01m
-
-bootstrap: 
-	@echo -e "${OK_COLOR}>>> Creating virtuelenv...${NO_COLOR}"
-	virtualenv --python=python2.7 --no-site-packages $(VENV)
-
-	@echo -e "${OK_COLOR}>>> Installing Ansible...${NO_COLOR}"
-	bash -c "source bin/activate && pip install ansible"
-
-	@echo -e "${OK_COLOR}>>> Installing vagrant-hostupdater ...${NO_COLOR}"
-	vagrant plugin install vagrant-hostsupdater
-
-	@echo -e "${OK_COLOR}Please activate your VirtualEnv, before you proceed!${NO_COLOR}"
+#!/bin/bash
+echo "installing git"
+sudo apt-get install git -y
+echo "installing ansible"
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get install ansible -y
+echo "installation is done"
